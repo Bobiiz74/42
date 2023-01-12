7a974819ae1c84cf47cbf4624fcc5f756fe8d888
Robin Godtschalck <rgodtsch@c1r10s3.42lausanne.ch>
2023-01-12 16:15:48 +0100

    nouvelle lib

---
+++ libft/ft_lstlast_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgodtsch <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 16:24:29 by rgodtsch          #+#    #+#             */
+/*   Updated: 2022/11/04 16:27:59 by rgodtsch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*tmp;
+
+	tmp = lst;
+	while (tmp)
+	{
+		if (!tmp->next)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
