2514a9b0b19af8e2e8c2477718c34451d61deb48
Robin Godtschalck <rgodtsch@c1r10s4.42lausanne.ch>
2023-01-30 17:26:44 +0100

    trunkil

--- fdf/fdf_key_event.c
+++ fdf/fdf_key_event.c
@@ -6,7 +6,7 @@
 /*   By: rgodtsch <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 14:15:41 by rgodtsch          #+#    #+#             */
-/*   Updated: 2023/01/30 17:14:50 by rgodtsch         ###   ########.fr       */
+/*   Updated: 2023/01/30 17:17:45 by rgodtsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,15 @@
 int	close_win(int keycode, t_vars *vars)
 {
 	if (keycode != 53)
-	{	
-		printf("%s\n", "coucou1");
 		mlx_destroy_window(vars->mlx, vars->win);
-	}
 	exit (0);
 	return (0);
 }
 
 static int	print_key(int keycode, t_vars *vars)
 {
-	(void) vars;
 	if (keycode == 53)
-	{
-		printf("%s\n", "coucou2");
 		close_win(keycode, vars);
-	}
 	return (0);
 }
 
